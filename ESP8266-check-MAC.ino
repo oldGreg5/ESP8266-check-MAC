d6293f6516f23622a8b1afa5c34a061f3d9a3516
ejajanu <janusz.janus@ericsson.com>
2016-09-19 14:00:07 +0200

    cleanup

--- ESP8266-check-MAC.ino
+++ ESP8266-check-MAC.ino
@@ -1,27 +1,25 @@
 #include <ESP8266WiFi.h>
-uint8_t  MAC_softAP[]          = {0,0,0,0,0,0};                      //not sure why we have to pass a MAC address to get a MAC address.
-uint8_t  MAC_STA[]                = {0,0,0,0,0,0};
+
+uint8_t MAC_array_STA[6];
+uint8_t MAC_array_AP[6];
+char MAC_char_STA[18];
+char MAC_char_AP[18];
 
 void setup() {
-    Serial.begin(115200);
-    Serial.println();
-    
-    Serial.print("MAC[SoftAP]");
-    uint8_t* MAC  = WiFi.softAPmacAddress(MAC_softAP);                   //get MAC address of softAP interface
-    for (int i = 0; i < sizeof(MAC)+2; ++i){                                                          //this line needs cleaning up.
-         Serial.print(":");
-         Serial.print(MAC[i],HEX);
-         MAC_softAP[i] = MAC[i];                                         //copy back to global variable
-    }
-    Serial.println();
-    Serial.print("MAC[STA]");
-    MAC  = WiFi.macAddress(MAC_STA);                   //get MAC address of STA interface
-    for (int i = 0; i < sizeof(MAC)+2; ++i){
-         Serial.print(":");
-         Serial.print(MAC[i],HEX);
-         MAC_STA[i] = MAC[i];                                            //copy back to global variable
-    }
-    Serial.println();
+	Serial.begin(115200);
+	Serial.println();
+
+	WiFi.macAddress(MAC_array_STA);
+	WiFi.softAPmacAddress(MAC_array_AP);
+	for (int i = 0; i < sizeof(MAC_array_STA); ++i) {
+		sprintf(MAC_char_STA, "%s%02x:", MAC_char_STA, MAC_array_STA[i]);
+		sprintf(MAC_char_AP, "%s%02x:", MAC_char_AP, MAC_array_AP[i]);
+	}
+
+	Serial.printf("MAC STA: ");
+	Serial.println(MAC_char_STA);
+	Serial.printf("MAC AP:  ");
+	Serial.println(MAC_char_AP);
 }
 
 void loop() {
